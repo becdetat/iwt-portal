9e2f389645d3ee3a478c09966f6c573e91e67d74
Ben Scott <ben.scott@readify.net>
2013-08-21 17:00:21 +1000

    added template project

---
+++ src/IwtPortal/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace IwtPortal
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ src/IwtPortal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IwtPortal.MvcApplication" Language="C#" %>
